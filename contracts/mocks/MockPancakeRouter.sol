9f726187bc68b2a26af578c735082c3b735560fc
KindoraHQ <154450659+KindoraHQ@users.noreply.github.com>
2025-12-17 07:36:23 +0100

    Create MockPancakeRouter.sol placeholder in contracts/mocks/

---
+++ contracts/mocks/MockPancakeRouter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// MockPancakeRouter
+// Placeholder Contract to Simulate Pancake Router Contract behavior
+
+contract MockPancakeRouter {
+    // Add your function placeholders here
+}
